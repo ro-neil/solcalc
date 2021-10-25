1c2e63638424d1c30c898da2ba6539dc2bb2cd4c
Your Naomi Benjamin <nobenjamin19@gmail.com>
2021-10-25 00:07:44 -0500

    Created calculator activity.

---
+++ app/src/main/java/com/example/solcalc/CalculatorActivity.kt
@@ -0,0 +1,35 @@
+package com.example.solcalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.slider.Slider
+import com.google.android.material.textfield.TextInputLayout
+
+class CalculatorActivity : AppCompatActivity() {
+
+    private lateinit var editName: TextInputLayout
+    private lateinit var editAddress: TextInputLayout
+    private lateinit var slider: Slider
+    private lateinit var batteries:TextInputLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+        editName = findViewById(R.id.client_name)
+        editAddress = findViewById(R.id.client_addr)
+
+        slider = findViewById(R.id.billCut)
+        slider.addOnChangeListener{slider, value, fromUser->
+            //gettingslider value
+        }
+
+        batteries = findViewById(R.id.if_batteries)
+        val getEstimate = findViewById<Button>(R.id.get_estimate)
+        getEstimate.setOnClickListener {
+            val name = editName.editText?.text.toString()
+            val address = editAddress.editText?.text.toString()
+        }
+
+    }
+}
