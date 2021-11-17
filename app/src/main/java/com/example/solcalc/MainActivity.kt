5645fbf5f2c015d32663d31103968fa30c6fa041
Your Naomi Benjamin <nobenjamin19@gmail.com>
2021-11-17 04:03:32 -0500

    Error in plan 1 main activity. Backup plan still works.

--- app/src/main/java/com/example/solcalc/MainActivity.kt
+++ app/src/main/java/com/example/solcalc/MainActivity.kt
@@ -16,79 +16,79 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_landing)
 
-        val editClientName: TextView = findViewById(R.id.ClientName)
-        val editClientAddress: TextView = findViewById(R.id.client_address)
-        val editClientTotal: TextView = findViewById(R.id.client_total)
-
-        val editClientName2: TextView = findViewById(R.id.ClientName2)
-        val editClientAddress2: TextView = findViewById(R.id.client_address2)
-        val editClientTotal2: TextView = findViewById(R.id.client_total2)
-
-        val editClientName3: TextView = findViewById(R.id.ClientName3)
-        val editClientAddress3: TextView = findViewById(R.id.client_address3)
-        val editClientTotal3: TextView = findViewById(R.id.client_total3)
-
-        estimateViewModel = ViewModelProvider(this@MainActivity).get(EstimateViewModel::class.java)
-
-        // Get Last Three Clients
-        val clients = estimateViewModel.getLastThreeClients()
-
-        if(clients.isNotEmpty()) {
-
-            // Store reference to Client 1 retrieved from DB
-            var clientName: String = clients[0].name
-            var clientAddress: String = clients[0].addr
-            var clientTotal: Double = estimateViewModel.getEstimate(clients[0].cid).totalCost
-
-            // Update UI with DB values
-            editClientName.text = clientName
-            editClientAddress.text = clientAddress
-            editClientTotal.text = clientTotal.toString()
-
-            try {
-                // Store reference to Client 2 retrieved from DB
-                clientName = clients[1].name
-                clientAddress = clients[1].addr
-                clientTotal = estimateViewModel.getEstimate(clients[1].cid).totalCost
-
-                // Update UI with DB values
-                editClientName2.text = clientName
-                editClientAddress2.text = clientAddress
-                editClientTotal2.text = clientTotal.toString()
-            } catch (e: Exception){
-                editClientName2.text = "-"
-                editClientAddress2.text = "-"
-                editClientTotal2.text = "-"
-            }
-
-            try {
-                // Store reference to Client 3 retrieved from DB
-                clientName = clients[2].name
-                clientAddress = clients[2].addr
-                clientTotal = estimateViewModel.getEstimate(clients[2].cid).totalCost
-
-                // Update UI with DB values
-                editClientName3.text = clientName
-                editClientAddress3.text = clientAddress
-                editClientTotal3.text = clientTotal.toString()
-            } catch(e: Exception){
-                editClientName3.text = "-"
-                editClientAddress3.text = "-"
-                editClientTotal3.text = "-"
-            }
-        } else {
-            editClientName.text = "-"
-            editClientAddress.text = "-"
-            editClientTotal.text = "-"
-            editClientName2.text = "-"
-            editClientAddress2.text = "-"
-            editClientTotal2.text = "-"
-            editClientName3.text = "-"
-            editClientAddress3.text = "-"
-            editClientTotal3.text = "-"
-        }
+//        val editClientName: TextView = findViewById(R.id.ClientName)
+//        val editClientAddress: TextView = findViewById(R.id.client_address)
+//        val editClientTotal: TextView = findViewById(R.id.client_total)
+//
+//        val editClientName2: TextView = findViewById(R.id.ClientName2)
+//        val editClientAddress2: TextView = findViewById(R.id.client_address2)
+//        val editClientTotal2: TextView = findViewById(R.id.client_total2)
+//
+//        val editClientName3: TextView = findViewById(R.id.ClientName3)
+//        val editClientAddress3: TextView = findViewById(R.id.client_address3)
+//        val editClientTotal3: TextView = findViewById(R.id.client_total3)
+//
+//        estimateViewModel = ViewModelProvider(this@MainActivity).get(EstimateViewModel::class.java)
+//
+//        // Get Last Three Clients
+//        val clients = estimateViewModel.getLastThreeClients()
+//
+//        if(clients.isNotEmpty()) {
+//
+//            // Store reference to Client 1 retrieved from DB
+//            var clientName: String = clients[0].name
+//            var clientAddress: String = clients[0].addr
+//            var clientTotal: Double = estimateViewModel.getEstimate(clients[0].cid).totalCost
+//
+//            // Update UI with DB values
+//            editClientName.text = clientName
+//            editClientAddress.text = clientAddress
+//            editClientTotal.text = clientTotal.toString()
+//
+//            try {
+//                // Store reference to Client 2 retrieved from DB
+//                clientName = clients[1].name
+//                clientAddress = clients[1].addr
+//                clientTotal = estimateViewModel.getEstimate(clients[1].cid).totalCost
+//
+//                // Update UI with DB values
+//                editClientName2.text = clientName
+//                editClientAddress2.text = clientAddress
+//                editClientTotal2.text = clientTotal.toString()
+//            } catch (e: Exception){
+//                editClientName2.text = "-"
+//                editClientAddress2.text = "-"
+//                editClientTotal2.text = "-"
+//            }
+//
+//            try {
+//                // Store reference to Client 3 retrieved from DB
+//                clientName = clients[2].name
+//                clientAddress = clients[2].addr
+//                clientTotal = estimateViewModel.getEstimate(clients[2].cid).totalCost
+//
+//                // Update UI with DB values
+//                editClientName3.text = clientName
+//                editClientAddress3.text = clientAddress
+//                editClientTotal3.text = clientTotal.toString()
+//            } catch(e: Exception){
+//                editClientName3.text = "-"
+//                editClientAddress3.text = "-"
+//                editClientTotal3.text = "-"
+//            }
+//        } else {
+//            editClientName.text = "-"
+//            editClientAddress.text = "-"
+//            editClientTotal.text = "-"
+//            editClientName2.text = "-"
+//            editClientAddress2.text = "-"
+//            editClientTotal2.text = "-"
+//            editClientName3.text = "-"
+//            editClientAddress3.text = "-"
+//            editClientTotal3.text = "-"
+//        }
 
 
         val calcBtn : Button = findViewById(R.id.calc_button)
